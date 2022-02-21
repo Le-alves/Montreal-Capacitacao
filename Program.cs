ef9e4c9fe1a0530886bdc562d6f983491b6d2087
Lê Alves <leticiaviz.alves@gmail.com>
2022-02-21 10:49:48 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main (string[] args)
         {
-            
+            //Entrada de dados para maior número
+                Console.WriteLine ("Digite os números que serão analisados: ");
+                string N1 = Console.ReadLine();
+                string N2 = Console.ReadLine();
+                string N3 = Console.ReadLine();
+                string N4 = Console.ReadLine();
+            Console.WriteLine ("Digite os números que serão analisados: ");
+            string N1 = Console.ReadLine();
+            string N2 = Console.ReadLine();
+            string N3 = Console.ReadLine();
+            string N4 = Console.ReadLine();
+            //Convertendo para números
+            int n1 = int.Parse(N1);
+            int n2 = int.Parse(N2);
+            int n3 = int.Parse(N3);
+            int n4 = int.Parse(N4);
+
         }
     }
 }
