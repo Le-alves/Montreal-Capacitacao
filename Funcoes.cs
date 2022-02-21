ede04c64792365514e2c454ef7e098ca680f2d67
Lê Alves <leticiaviz.alves@gmail.com>
2022-02-21 13:17:34 -0300

    Alteracoes

--- Funcoes.cs
+++ Funcoes.cs
@@ -3,38 +3,14 @@ namespace AtivSem01
     class Funcoes
     {
         //Função parametro
-        public int MaiorNumero (int a, int b, int c, int d)
+        public int Num1, Num2, Num3, Num4;
+
+        public int MaiorNumero (Num1, Num2, Num3, Num4)
         {
-            int armzenamento1;
-            int armzenamento2;
-            int armzenamentoMaior;
+            int numero;
+             
 
-            if (a > b){
-                armzenamento1 = a;
-            
-            }else{
-            
-                armzenamento1 = b;
-            }
-            if (c > d) 
-            {
-                armzenamento2 = b;
-            }
-            else
-            {
-                armzenamento2 = d;
-            }
-
-            if (armzenamento1 > armzenamento2) 
-            {
-                armzenamento1 = armzenamentoMaior;
-            }
-            else
-            {
-                armzenamento2 = armzenamentoMaior;
-            }            
-
-            return armzenamentoMaior;
-        }
+            return numero;
+        }       
     }
 }
